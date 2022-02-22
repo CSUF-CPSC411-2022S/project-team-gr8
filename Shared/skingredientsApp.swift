22bd462bf08549245c9ccabd2515d771a627af02
Johnson Tong <tong.johnson.28@gmail.com>
2022-02-21 16:34:25 -0800

    xcode project created with gitignore

---
+++ Shared/skingredientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  skingredientsApp.swift
+//  Shared
+//
+//  Created by csuftitan on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct skingredientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
