0cfac6b8df09eb245fec6e756946f97aeb4989fb
Tristan Stewart <stewarttristan844@csu.fullerton.edu>
2022-02-22 21:47:51 -0800

    featuring warning function

---
+++ Shared/warnings_feature.swift
@@ -0,0 +1,27 @@
+//
+//  warnings_feature.swift
+//  skingredients
+//
+//  Created by csuftitan on 2/22/22.
+//
+
+import Foundation
+
+class warnings {
+    
+    
+    func isHarmful(using filter: ([String]) -> [String]){
+        var harmful = false
+        //for i in 0..self.productingredients.count-1
+        //  if an ingredient is found to be harmful
+        //      print("harmful ingredient: \(ingredient name)")
+        //      harmful = true
+        //
+        return harmful = false
+    }
+    
+    //func badratings
+    //would check the ratings from online reviews
+    //sending a warning message
+    
+}
