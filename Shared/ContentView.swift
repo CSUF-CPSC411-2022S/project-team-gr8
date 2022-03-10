2037e5d58f0552f3761702ec1cc4788aeb4baeca
Johnson Tong <tong.johnson.28@gmail.com>
2022-03-10 10:13:24 -0800

    Revert "Updated Tabview with new Amazon and Google Buttons, created amazon-prices class and unit tests that will both need to be finished"
    This reverts commit a7a3906d6148f12353a70ec1c0ff9f5e10ba9e4c.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -3,184 +3,19 @@
 //  Shared
 //
 //  Created by csuftitan on 2/21/22.
-//  using Patricia's tabview to test Amazon-prices and Google-Search methods
+//
 
 import SwiftUI
 
 struct ContentView: View {
-    @State var data: String = ""
-    @ObservedObject var db = ProductsDatabase()
-    
-    // Tab view, Ref: https://www.youtube.com/watch?v=9IVLFlyaiq4
-    
-    @State var selectedIndex = 0
-    @State var shouldShowModal = false
-    
-    // House = 1, Magnifyingglass AR = 2 , Magnifyingglass Text = 3,
-    let tabBarImagesNames = ["house", "1.magnifyingglass.ar", "text.magnifyingglass"]
-
     var body: some View {
-        // Tab view, Ref: https://www.youtube.com/watch?v=9IVLFlyaiq4
-        VStack(spacing: 0){
-            ZStack {
-                Spacer()
-                // AR Page
-                    .fullScreenCover(isPresented: $shouldShowModal, content: {
-                        // Dismiss button
-                        Button(action: {shouldShowModal.toggle()}, label: {
-                            Image(systemName: "x.circle.fill")
-                                .foregroundColor(.orange)
-                        })
-                        Spacer()
-                        
-                        NavigationView {
-                            
-                            ScrollView {
-                                Text("Insert AR Content")
-                                    .italic()
-                            }
-                            .navigationTitle("AR Search")
-                        }
-                        
-                    })
-                
-                
-           // Controls Different Pages
-                switch selectedIndex {
-                case 0:
-                // Database View --- HOME Button
-                    NavigationView{
-                        VStack {
-                            Text("Welcome to SkinGredients!")
-                                .font(.system(size: 20, weight: .semibold))
-                                .foregroundColor(.orange)
-                            List(db.items, id: \.id) { item in
-                                HStack {
-                                    Text("DB ID: ").bold()
-                                    Text(String(item.id))
-                                }
-                                HStack {
-                                    Text("Brand: ").bold()
-                                    Text(item.brand)
-                                }
-                                HStack {
-                                    Text("Product: ").bold()
-                                    Text(item.name)
-                                }
-                                HStack {
-                                    Text("Ingredients: ").bold()
-                                    Text(item.ingredient_list.joined(separator: ", "))
-                                }
-                                HStack {
-                                    // button for amazon
-                                    Button(action: {Amazon()}){
-                                        Text("Click for Amazon")
-                                    }.buttonStyle(PlainButtonStyle())
-                                }
-                                HStack {
-                                    // button for google
-                                    Button(action: {GSearch()}){
-                                        Text("Click for Google")
-                                    }.buttonStyle(PlainButtonStyle())
-                                }
-                            }
-                            
-                        }.onAppear(perform: {
-                            db.getAllData()
-                        }).navigationTitle("SkinGredients")
-
-                    }
-                case 1:
-                // AR View
-                    NavigationView {
-                        Text("AR Search")
-                            .navigationTitle("AR Search")
-                    }
-                case 2:
-                // Search Text View
-                    NavigationView {
-                        ScrollView{
-                            Text("Insert Search Bar")
-                                .italic()
-                                
-                        }
-                        .navigationTitle("Text Search")
-                    }
-                default:
-                    NavigationView{
-                        Text("Remaining Tabs")
-                    }
-
-                }
-            }
-            
-            // Spacer()
-            // Divider to separate Content and Nav Bar
-            Divider()
-                .padding(.bottom, 8)
-            
-            
-            HStack {
-                ForEach(0..<3) { num in
-                    Button(action: {
-                        // Actions for the buttons
-                        if num == 1 {                   // AR Search Button
-                            shouldShowModal.toggle()
-                            return
-                        }
-                        
-                        selectedIndex = num
-                    }, label: {
-                        Spacer()
-                        
-                        // Custom Button Display
-                        if num == 1 {               // AR Search Button
-                            Image(systemName: tabBarImagesNames[num])
-                                .font(.system(size: 35, weight: .bold))
-                                .foregroundColor(.orange)
-                            
-                        } else {                    // For other buttons
-                            Image(systemName: tabBarImagesNames[num])
-                                .font(.system(size: 25, weight: .bold))
-                                .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
-                        }
-                        
-                        Spacer()
-                        
-                    })
-                    
-                }
-            }
-            
-        }
-        
+        Text("Hello, world!")
+            .padding()
     }
-        
 }
-
-// TODO: Need a tabview with a search page, and an AR page for the user. This is mainly
-//       just user interface for now. Check out https://www.youtube.com/watch?v=TgvYFfCjDMo
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-// The url functions appear to work better when under ContenView rather than their seperate class
-// at least for now...
-
-// opens amazon
-func Amazon(){
-    
-    if let url = URL(string: "https://www.amazon.com"){
-        UIApplication.shared.open(url)
-    }
-}
-
-// opens google
-func GSearch(){
-    if let url = URL(string: "https://www.google.com"){
-        UIApplication.shared.open(url)
     }
 }
