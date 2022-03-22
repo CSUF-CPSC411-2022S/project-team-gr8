f2a65a1fc859b1c4b99ed43038f1a97c67837aa5
Johnson Tong <tong.johnson.28@gmail.com>
2022-03-22 11:07:00 -0700

    feat(tabview + data api call)

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var data: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        // Tab view, Ref: https://www.youtube.com/watch?v=9IVLFlyaiq4
+        VStack(spacing: 0) {
+             
+            // Controls the tab views using a switch statement.
+            switchView()
+            
+        } // VStack (spacing: 0)
+        
+    } // var body : some View
+        
+} // struct ContentView : View
+
+// TODO: Need a tabview with a search page, and an AR page for the user. This is mainly
+//       just user interface for now. Check out https://www.youtube.com/watch?v=TgvYFfCjDMo
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
---
+++ Shared/TabView.swift
@@ -0,0 +1,162 @@
+//
+//  TabView.swift
+//  skingredients (iOS)
+//
+//  Created by csuftitan on 3/10/22.
+
+import Foundation
+import SwiftUI
+
+class TabView : ObservableObject {
+    @Published var selectedIndex = 0
+    @Published var isPresented = false //shouldShowModal
+    
+    // House = 1, Magnifyingglass AR = 2 , Magnifyingglass Text = 3,
+    let tabBarImagesNames = ["house", "1.magnifyingglass.ar", "text.magnifyingglass"]
+   
+}
+
+// Creates a FullScreenModal View for the AR Search.
+struct FullScreenModalView : View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject var tV = TabView()
+    
+    var body : some View {
+        Spacer() // Needed for x button to appear
+        VStack{
+            Button(action: {tV.isPresented.toggle()}, label: {
+                 Image(systemName: "x.circle.fill")
+                     .foregroundColor(.orange)
+                     .font(.system(size: 20, weight: .bold))
+                     .onTapGesture{
+                         presentationMode.wrappedValue.dismiss()
+                     }
+             })
+            
+            NavigationView {
+                    ScrollView {
+                        Text("Insert AR Content")
+                            .italic()
+                    }
+                    .navigationTitle("AR Search")
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+}
+
+// Creates how the database is presented.
+struct databaseView : View {
+    @ObservedObject var db = ProductsDatabase()
+    
+    var body : some View {
+        VStack {
+            Text("Welcome to SkinGredients!")
+                .font(.system(size: 20, weight: .semibold))
+                .foregroundColor(.orange)
+            List(db.items, id: \.id) { item in
+                HStack {
+                    Text("DB ID: ").bold()
+                    Text(String(item.id))
+                }
+                HStack {
+                    Text("Brand: ").bold()
+                    Text(item.brand)
+                }
+                HStack {
+                    Text("Product: ").bold()
+                    Text(item.name)
+                }
+                HStack {
+                    Text("Ingredients: ").bold()
+                    Text(item.ingredient_list.joined(separator: ", "))
+                }
+            }
+            
+        }.onAppear(perform: {
+            db.getAllData()
+        }).navigationTitle("SkinGredients")
+
+    }
+}
+
+// Creates and Controls each tab in the navigation. 
+struct switchView : View {
+    @ObservedObject var tV = TabView()
+    
+    var body: some View {
+        ZStack {
+            Spacer() // Needed for fullScreenCover to work
+                .fullScreenCover(isPresented: $tV.isPresented, content: FullScreenModalView.init)
+            
+            switch tV.selectedIndex {
+            case 0:
+                NavigationView {
+                    // Call databaseView to display database display format
+                    databaseView()
+                }
+            case 1:
+            // AR View
+                NavigationView {
+                    Text("AR Search")
+                        .navigationTitle("AR Search")
+                }
+            case 2:
+            // Search Text View
+                NavigationView {
+                    ScrollView{
+                        Text("Insert Search Bar")
+                            .italic()
+                            
+                    }
+                    .navigationTitle("Text Search")
+                }
+            default:
+                NavigationView{
+                    Text("Remaining Tabs")
+                }
+            } // Switch statement
+            
+        } // ZStack
+        
+        // Divider to separate Content and Nav Bar
+        Divider()
+            .padding(.bottom, 8)
+        
+        // Setting up the 3 control buttons.
+        HStack {
+            ForEach(0..<3) { num in
+                Button(action: {
+                    // Actions for the buttons
+                    if num == 1 {                   // AR Search Button
+                        tV.isPresented.toggle()
+                        return
+                    }
+                    
+                    tV.selectedIndex = num
+                }, label: {
+                    Spacer()
+                    
+                    // Custom Button Display
+                    if num == 1 {               // AR Search Button
+                        Image(systemName: tV.tabBarImagesNames[num])
+                            .font(.system(size: 35, weight: .bold))
+                            .foregroundColor(.orange)
+                        
+                    } else {                    // For other buttons
+                        Image(systemName: tV.tabBarImagesNames[num])
+                            .font(.system(size: 25, weight: .bold))
+                            .foregroundColor(tV.selectedIndex == num ? Color(.black) : .init(white: 0.8))
+                    }
+                    
+                    Spacer()
+                    
+                }) // Button
+            } // ForEach
+        } // HStack
+        
+    } // View
+} // struct switchView : View
